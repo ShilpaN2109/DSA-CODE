d1b3acfcafd0e031298a3a787e30d01b7ec8a5b4
ShilpaN2109 <shilpa.nagre21@gmail.com>
2024-06-16 09:46:02 +0530

    Update Hash MAp.py
    Code using Mapping

--- Hash MAp.py
+++ Hash MAp.py
@@ -1,1 +1,12 @@
-Code
+import collections
+n = int(input())
+arr = [int(x) for x in input().split()]
+
+# precompute
+mp = collections.Counter(arr)
+
+
+q = int(input())
+for _ in range(q):
+    number = int(input())
+    print(mp[number])
